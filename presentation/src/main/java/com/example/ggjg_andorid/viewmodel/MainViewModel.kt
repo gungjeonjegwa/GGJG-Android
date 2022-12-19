7bd309ce03992a66304d05b3a8d8a513ee1fc967
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-19 19:48:48 +0900

    메인 네비게이션 숨기는 기능 추가

---
+++ presentation/src/main/java/com/example/ggjg_andorid/viewmodel/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.ggjg_andorid.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ggjg_andorid.utils.MutableEventFlow
+import com.example.ggjg_andorid.utils.asEventFlow
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+
+): ViewModel() {
+
+    private val _eventFlow = MutableEventFlow<Event>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    fun hiddenNav(status: Boolean) {
+        event(Event.IsHiddenNav(status))
+    }
+
+    private fun event(event: Event) = viewModelScope.launch {
+        _eventFlow.emit(event)
+    }
+    sealed class Event {
+        data class IsHiddenNav(val status: Boolean): Event()
+    }
+}
