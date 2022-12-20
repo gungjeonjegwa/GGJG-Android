ac2b9ecc572699bbe7a386e8a088e5244113d20c
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-20 11:09:18 +0900

    키보드 숨길 때 editText focus 없애기

--- presentation/src/main/java/com/example/ggjg_andorid/utils/KeyboardEvent.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/utils/KeyboardEvent.kt
@@ -3,14 +3,16 @@ package com.example.ggjg_andorid.utils
 import android.app.Activity
 import android.content.Context
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 
-fun keyboardHide(context: Activity) {
+fun keyboardHide(context: Activity, view: EditText) {
     val inputMethodManager =
         context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(
         context.currentFocus?.windowToken,
         InputMethodManager.HIDE_NOT_ALWAYS
     )
+    view.clearFocus()
 }
 
 fun keyboardShow(context: Activity) {
