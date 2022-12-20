e9877842aac0258e3039652f9fc71791f6bb043a
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-20 11:15:17 +0900

    키보드 내려가는 기능 구현

--- presentation/src/main/java/com/example/ggjg_andorid/ui/search/SearchFragment.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/search/SearchFragment.kt
@@ -1,11 +1,33 @@
 package com.example.ggjg_andorid.ui.search
 
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
 import com.example.ggjg_andorid.R
 import com.example.ggjg_andorid.databinding.FragmentSearchBinding
 import com.example.ggjg_andorid.ui.base.BaseFragment
+import com.example.ggjg_andorid.utils.keyboardHide
+import com.example.ggjg_andorid.viewmodel.MainViewModel
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    private val mainViewModel by activityViewModels<MainViewModel>()
     override fun createView() {
+        binding.search = this
+        mainViewModel.hiddenNav(true)
+    }
 
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.backBtn -> {
+                mainViewModel.hiddenNav(false)
+                requireActivity().findNavController(R.id.mainContainer).popBackStack()
+            }
+            R.id.searchLayout -> {
+                keyboardHide(requireActivity(), binding.searchBread)
+            }
+            R.id.searchBtn -> {
+                keyboardHide(requireActivity(), binding.searchBread)
+            }
+        }
     }
 }
