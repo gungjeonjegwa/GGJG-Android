5a4a61d43ff395e9472655a9aaabbc1c86b9d68e
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-20 13:49:13 +0900

    RoomModule 작성

---
+++ presentation/src/main/java/com/example/ggjg_andorid/di/RoomModule.kt
@@ -0,0 +1,21 @@
+package com.example.ggjg_andorid.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.data.local.dao.RecentSearchDao
+import com.example.data.local.database.GGJGDataBase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@Module
+@InstallIn
+object RoomModule {
+    @Provides
+    fun provideGGJGDataBase(
+        @ApplicationContext context: Context
+    ): GGJGDataBase = Room
+        .databaseBuilder(context, GGJGDataBase::class.java, "GGJGDataBase")
+        .build()
+}
