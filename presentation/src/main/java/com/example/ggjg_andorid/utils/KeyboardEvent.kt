a7bc539c0efbd30ac80e15b39b571635855f9c9a
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-21 10:06:32 +0900

    키보드 관련 함수 변경

--- presentation/src/main/java/com/example/ggjg_andorid/utils/KeyboardEvent.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/utils/KeyboardEvent.kt
@@ -1,25 +1,18 @@
 package com.example.ggjg_andorid.utils
 
 import android.app.Activity
-import android.content.Context
-import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 fun keyboardHide(context: Activity, view: EditText) {
-    val inputMethodManager =
-        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    inputMethodManager.hideSoftInputFromWindow(
-        context.currentFocus?.windowToken,
-        InputMethodManager.HIDE_NOT_ALWAYS
-    )
     view.clearFocus()
+    val window = context.window
+    WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.ime())
 }
 
-fun keyboardShow(context: Activity) {
-    val inputMethodManager =
-        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    inputMethodManager.toggleSoftInput(
-        InputMethodManager.SHOW_FORCED,
-        InputMethodManager.HIDE_IMPLICIT_ONLY
-    )
+fun keyboardShow(context: Activity, view: EditText) {
+    view.requestFocus()
+    val window = context.window
+    WindowInsetsControllerCompat(window, window.decorView).show(WindowInsetsCompat.Type.ime())
 }
