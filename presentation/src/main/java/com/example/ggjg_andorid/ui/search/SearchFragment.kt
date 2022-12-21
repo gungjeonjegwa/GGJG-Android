13cda0d094eb6a63c2118d3b40943634c85e1ac0
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-21 10:07:22 +0900

    검색 창 시작 시 포커스 및 밑줄 구현

--- presentation/src/main/java/com/example/ggjg_andorid/ui/search/SearchFragment.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/search/SearchFragment.kt
@@ -1,20 +1,15 @@
 package com.example.ggjg_andorid.ui.search
 
 import android.view.View
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.example.ggjg_andorid.R
-import com.example.ggjg_andorid.adapter.RecentSearchAdapter
 import com.example.ggjg_andorid.databinding.FragmentSearchBinding
 import com.example.ggjg_andorid.ui.base.BaseFragment
-import com.example.ggjg_andorid.utils.keyboardHide
-import com.example.ggjg_andorid.utils.repeatOnStart
-import com.example.ggjg_andorid.utils.setOnTextChanged
+import com.example.ggjg_andorid.utils.*
 import com.example.ggjg_andorid.viewmodel.MainViewModel
 import com.example.ggjg_andorid.viewmodel.SearchViewModel
-import com.google.android.flexbox.FlexboxLayoutManager
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     private val mainViewModel by activityViewModels<MainViewModel>()
@@ -29,6 +24,7 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
         when (view.id) {
             R.id.backBtn -> {
                 mainViewModel.hiddenNav(false)
+                keyboardHide(requireActivity(), binding.searchBread)
                 requireActivity().findNavController(R.id.mainContainer).popBackStack()
             }
             R.id.searchLayout -> {
@@ -44,11 +40,21 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
 
     private fun initView() = binding.apply {
         viewFragment(SearchRecentFragment())
-        searchBread.setOnTextChanged { p0, _, _, _ ->
-            if (p0.isNullOrBlank()) {
-                viewFragment(SearchRecentFragment())
-            } else {
-                viewFragment(SearchingFragment())
+        keyboardShow(requireActivity(), binding.searchBread)
+        searchBread.run {
+            setOnTextChanged { p0, _, _, _ ->
+                if (p0.isNullOrBlank()) {
+                    viewFragment(SearchRecentFragment())
+                } else {
+                    viewFragment(SearchingFragment())
+                }
+            }
+            setOnFocusChangeListener { _, b ->
+                if (b) {
+                    binding.searchDivide.setVisible()
+                } else {
+                    binding.searchDivide.setVisible(false)
+                }
             }
         }
     }
