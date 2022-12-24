2d27b2f9fa7bcaa4c798d6e98d3df3230d94136c
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-24 15:22:35 +0900

    마이페이지 진입시 로그인으로 이동

--- presentation/src/main/java/com/example/ggjg_andorid/ui/my_page/MyPageFragment.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/my_page/MyPageFragment.kt
@@ -1,11 +1,15 @@
 package com.example.ggjg_andorid.ui.my_page
 
+import android.content.Intent
+import androidx.navigation.findNavController
 import com.example.ggjg_andorid.R
 import com.example.ggjg_andorid.databinding.FragmentMyPageBinding
 import com.example.ggjg_andorid.ui.base.BaseFragment
+import com.example.ggjg_andorid.ui.login.LoginActivity
 
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     override fun createView() {
-
+        requireActivity().findNavController(R.id.mainContainer).popBackStack()
+        requireActivity().startActivity(Intent(requireActivity(), LoginActivity::class.java))
     }
 }
