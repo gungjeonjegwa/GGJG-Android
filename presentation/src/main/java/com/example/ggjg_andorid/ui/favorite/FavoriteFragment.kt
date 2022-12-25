95bb79dfb54f21149a07f1f4f1ff55a5f9d01d28
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-26 00:11:08 +0900

    관심상품과 장바구니에서 로그인으로 이동

--- presentation/src/main/java/com/example/ggjg_andorid/ui/favorite/FavoriteFragment.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/favorite/FavoriteFragment.kt
@@ -1,11 +1,28 @@
 package com.example.ggjg_andorid.ui.favorite
 
+import android.content.Intent
+import androidx.navigation.findNavController
 import com.example.ggjg_andorid.R
 import com.example.ggjg_andorid.databinding.FragmentFavoriteBinding
 import com.example.ggjg_andorid.ui.base.BaseFragment
+import com.example.ggjg_andorid.ui.login.LoginActivity
 
 class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(R.layout.fragment_favorite) {
     override fun createView() {
+        this.startActivityForResult(
+            Intent(
+                requireActivity(),
+                LoginActivity::class.java
+            ), 0
+        )
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != 1) {
+            requireActivity().findNavController(R.id.mainContainer).popBackStack()
+        } else {
+
+        }
     }
 }
--- presentation/src/main/java/com/example/ggjg_andorid/ui/shopping_list/ShoppingListFragment.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/shopping_list/ShoppingListFragment.kt
@@ -1,12 +1,29 @@
 package com.example.ggjg_andorid.ui.shopping_list
 
+import android.content.Intent
+import androidx.navigation.findNavController
 import com.example.ggjg_andorid.R
 import com.example.ggjg_andorid.databinding.FragmentShoppingListBinding
 import com.example.ggjg_andorid.ui.base.BaseFragment
+import com.example.ggjg_andorid.ui.login.LoginActivity
 
 class ShoppingListFragment :
     BaseFragment<FragmentShoppingListBinding>(R.layout.fragment_shopping_list) {
     override fun createView() {
+        this.startActivityForResult(
+            Intent(
+                requireActivity(),
+                LoginActivity::class.java
+            ), 0
+        )
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != 1) {
+            requireActivity().findNavController(R.id.mainContainer).popBackStack()
+        } else {
+
+        }
     }
 }
