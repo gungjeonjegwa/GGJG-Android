fbc7823cf10d648659bba249d2762fa58b949394
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-27 00:26:38 +0900

    로그인 여부에 따라 구매 다이얼로그 띄우기

--- presentation/src/main/java/com/example/ggjg_andorid/ui/detail/DetailBreadFragment.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/detail/DetailBreadFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.example.ggjg_andorid.R
 import com.example.ggjg_andorid.adapter.BannerAdapter
@@ -13,6 +14,7 @@ import com.example.ggjg_andorid.databinding.ActivityMainBinding
 import com.example.ggjg_andorid.databinding.FragmentDetailBreadBinding
 import com.example.ggjg_andorid.ui.base.BaseActivity
 import com.example.ggjg_andorid.ui.base.BaseFragment
+import com.example.ggjg_andorid.ui.login.LoginActivity
 import com.example.ggjg_andorid.ui.main.MainActivity
 import com.example.ggjg_andorid.utils.changeActivatedWithEnabled
 import com.example.ggjg_andorid.utils.heartChange
@@ -20,6 +22,7 @@ import com.example.ggjg_andorid.utils.repeatOnStart
 import com.example.ggjg_andorid.utils.setVisible
 import com.example.ggjg_andorid.viewmodel.DetailViewModel
 import com.example.ggjg_andorid.viewmodel.MainViewModel
+import com.example.ggjg_andorid.viewmodel.PayViewModel
 import java.text.DecimalFormat
 
 class DetailBreadFragment :
@@ -56,7 +59,23 @@ class DetailBreadFragment :
     }
 
     private fun handleEvent(event: DetailViewModel.Event) = when (event) {
+        is DetailViewModel.Event.IsLogin -> {
+            if (!event.isLogin) {
+                this.startActivityForResult(
+                    Intent(
+                        requireActivity(),
+                        LoginActivity::class.java
+                    ), 0
+                )
+            } else {
+                DetailBreadPayFragment().show(
+                    requireActivity().supportFragmentManager,
+                    "DetailBreadPayFragment"
+                )
+            }
+        }
         is DetailViewModel.Event.DetailBread -> {
+            PayViewModel.breadData = event.detailBread
             binding.apply {
                 breadData = event.detailBread
                 if (event.detailBread.isSoldOut) {
@@ -149,10 +168,31 @@ class DetailBreadFragment :
                     )
                 )
             }
+            R.id.changeAndRecallInfoBtn -> {
+                requireActivity().startActivity(
+                    Intent(
+                        context,
+                        DetailChangeAndRecallActivity::class.java
+                    )
+                )
+            }
             R.id.likeBtn -> {
                 binding.likeBtn.isActivated = !binding.likeBtn.isActivated
                 binding.like.heartChange(binding.likeBtn.isActivated, requireContext())
             }
+            R.id.payBtn -> {
+                detailViewModel.isLogin()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == 1) {
+            DetailBreadPayFragment().show(
+                requireActivity().supportFragmentManager,
+                "DetailBreadPayFragment"
+            )
         }
     }
 }
