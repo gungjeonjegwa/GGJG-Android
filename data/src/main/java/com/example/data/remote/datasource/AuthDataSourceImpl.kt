3e7eb675ff1387bb388601c9155cd4373b11740c
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-28 22:23:16 +0900

    dataSource에 HttpHandler 적용

--- data/src/main/java/com/example/data/remote/datasource/AuthDataSourceImpl.kt
+++ data/src/main/java/com/example/data/remote/datasource/AuthDataSourceImpl.kt
@@ -5,23 +5,34 @@ import com.example.data.remote.request.auth.LoginRequest
 import com.example.data.remote.request.auth.SignUpRequest
 import com.example.data.remote.response.auth.CheckResponse
 import com.example.data.remote.response.auth.LoginResponse
+import com.example.data.utils.HttpHandler
 import javax.inject.Inject
 
 class AuthDataSourceImpl @Inject constructor(
-    private val authAPI: AuthAPI
+    private val authAPI: AuthAPI,
 ) : AuthDataSource {
     override suspend fun signUp(signUpRequest: SignUpRequest) =
-        authAPI.signUp(signUpRequest)
+        HttpHandler<Unit>()
+            .httpRequest { authAPI.signUp(signUpRequest) }
+            .sendRequest()
 
     override suspend fun login(loginRequest: LoginRequest): LoginResponse =
-        authAPI.login(loginRequest)
+        HttpHandler<LoginResponse>()
+            .httpRequest { authAPI.login(loginRequest) }
+            .sendRequest()
 
     override suspend fun logout() =
-        authAPI.logout()
+        HttpHandler<Unit>()
+            .httpRequest { authAPI.logout() }
+            .sendRequest()
 
     override suspend fun idCheck(id: String): CheckResponse =
-        authAPI.idCheck(id)
+        HttpHandler<CheckResponse>()
+            .httpRequest { authAPI.idCheck(id) }
+            .sendRequest()
 
     override suspend fun emailCheck(email: String): CheckResponse =
-        authAPI.emailCheck(email)
+        HttpHandler<CheckResponse>()
+            .httpRequest { authAPI.emailCheck(email) }
+            .sendRequest()
 }
--- data/src/main/java/com/example/data/remote/datasource/BasketDataSourceImpl.kt
+++ data/src/main/java/com/example/data/remote/datasource/BasketDataSourceImpl.kt
@@ -3,23 +3,34 @@ package com.example.data.remote.datasource
 import com.example.data.remote.api.BasketAPI
 import com.example.data.remote.request.basket.MakeBasketRequest
 import com.example.data.remote.response.basket.MyBasketResponse
+import com.example.data.utils.HttpHandler
 import javax.inject.Inject
 
 class BasketDataSourceImpl @Inject constructor(
     private val basketAPI: BasketAPI,
 ) : BasketDataSource {
     override suspend fun myBasket(): List<MyBasketResponse> =
-        basketAPI.myBasket()
+        HttpHandler<List<MyBasketResponse>>()
+            .httpRequest { basketAPI.myBasket() }
+            .sendRequest()
 
     override suspend fun deleteBasket(id: String) =
-        basketAPI.deleteBasket(id)
+        HttpHandler<Unit>()
+            .httpRequest { basketAPI.deleteBasket(id) }
+            .sendRequest()
 
     override suspend fun plusBasket(id: String) =
-        basketAPI.plusBasket(id)
+        HttpHandler<Unit>()
+            .httpRequest { basketAPI.plusBasket(id) }
+            .sendRequest()
 
     override suspend fun minusBasket(id: String) =
-        basketAPI.minusBasket(id)
+        HttpHandler<Unit>()
+            .httpRequest { basketAPI.minusBasket(id) }
+            .sendRequest()
 
     override suspend fun makeBasket(makeBasketRequest: MakeBasketRequest) =
-        basketAPI.makeBasket(makeBasketRequest)
+        HttpHandler<Unit>()
+            .httpRequest { basketAPI.makeBasket(makeBasketRequest) }
+            .sendRequest()
 }
