3d7dd15abe3744dca3b92e6c891b8b1b34eae851
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-28 22:22:37 +0900

    Exception 세팅

---
+++ domain/src/main/java/com/example/domain/exception/ExeceptionList.kt
@@ -0,0 +1,7 @@
+package com.example.domain.exception
+
+class MoreDataException: RuntimeException()
+class TokenErrorException: RuntimeException()
+class NotFoundException: RuntimeException()
+class ConflictException: RuntimeException()
+class UnknownException: RuntimeException()
