a45cd346ee0d72f8eb7180994ceae9aec34c83f5
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-28 22:23:00 +0900

    에러 코드에 따라 Excepetion throw

--- data/src/main/java/com/example/data/utils/HttpHandler.kt
+++ data/src/main/java/com/example/data/utils/HttpHandler.kt
@@ -1,21 +1,44 @@
 package com.example.data.utils
 
+import com.example.domain.exception.*
 import retrofit2.HttpException
 
 class HttpHandler<T> {
     private lateinit var httpRequest: suspend () -> T
-    private var errorStatus: (code: Int, message: String) -> Throwable = {_, _ -> RuntimeException()}
+    private var onMoreData: (message: String) -> Throwable = { MoreDataException() }
+    private var onTokenError: (message: String) -> Throwable = { TokenErrorException() }
+    private var onNotFound: (message: String) -> Throwable = { NotFoundException() }
+    private var onConflict: (message: String) -> Throwable = { ConflictException() }
+    private var onOtherHttpStatusCode: (code: Int, message: String) -> Throwable = { _, _ -> UnknownException() }
 
     fun httpRequest(httpRequest: suspend () -> T) =
         this.apply { this.httpRequest = httpRequest }
 
-    fun onErrorStatus(errorStatus: (code: Int, message: String) -> Throwable) =
-        this.apply { this.errorStatus = errorStatus }
+    fun onMoreData(onMoreData: (message: String) -> Throwable) =
+        this.apply { this.onMoreData = onMoreData }
+
+    fun onTokenError(onTokenError: (message: String) -> Throwable) =
+        this.apply { this.onTokenError = onTokenError }
+
+    fun onNotFound(onNotFound: (message: String) -> Throwable) =
+        this.apply { this.onNotFound = onNotFound }
+
+    fun onConflict(onConflict: (message: String) -> Throwable) =
+        this.apply { this.onConflict = onConflict }
+
+    fun onOtherHttpStatusCode(onOtherHttpStatusCode: (code: Int, message: String) -> Throwable) =
+        this.apply { this.onOtherHttpStatusCode = onOtherHttpStatusCode }
 
     suspend fun sendRequest(): T =
         try {
             httpRequest()
         } catch (e: HttpException) {
-            throw errorStatus(e.code(), e.message())
+            throw when (e.code()) {
+                400 -> onMoreData(e.message())
+                401 -> onTokenError(e.message())
+                404 -> onNotFound(e.message())
+                409 -> onConflict(e.message())
+                else -> onOtherHttpStatusCode(e.code(), e.message())
+            }
         }
 }
