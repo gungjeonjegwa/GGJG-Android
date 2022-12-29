352a162b7789cb5b3cea4484b86705c598bef97c
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-29 19:00:55 +0900

    백키 리스너 제거

--- presentation/src/main/java/com/example/ggjg_andorid/ui/base/BaseActivity.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/base/BaseActivity.kt
@@ -6,41 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity<B: ViewDataBinding>(
-    @LayoutRes private val layoutId: Int
-): AppCompatActivity() {
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int,
+) : AppCompatActivity() {
 
     protected lateinit var binding: B
-    private var onKeyBackPressedListener: OnKeyBackPressedListener? = null
-
-    interface OnKeyBackPressedListener {
-        fun onBack()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this,layoutId)
+        binding = DataBindingUtil.setContentView(this, layoutId)
         binding.lifecycleOwner = this
 
         createView()
     }
 
-    override fun onBackPressed() {
-        if (onKeyBackPressedListener != null) {
-            (onKeyBackPressedListener as OnKeyBackPressedListener).onBack()
-        } else {
-            super.onBackPressed()
-        }
-    }
-
     abstract fun createView()
-
-    fun setOnKeyBackPressedListener(listener: OnKeyBackPressedListener) {
-        onKeyBackPressedListener = listener
-    }
-
-    fun deleteOnKeyBackPressedListener() {
-        onKeyBackPressedListener = null
-    }
 }
