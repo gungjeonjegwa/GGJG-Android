dbcf965f6668bf532fdc940b686a94edbb3126f3
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-29 17:26:00 +0900

    주소 검색 string 추가

---
+++ presentation/src/main/java/com/example/ggjg_andorid/adapter/AddressAdapter.kt
@@ -0,0 +1,75 @@
+package com.example.ggjg_andorid.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ggjg_andorid.R
+import com.example.ggjg_andorid.databinding.ItemAddressBinding
+import com.example.ggjg_andorid.databinding.ItemPayOptionBinding
+import com.example.ggjg_andorid.viewmodel.AddressViewModel
+
+class AddressAdapter :
+    ListAdapter<AddressViewModel.Address, AddressAdapter.AddressViewHolder>(
+        diffUtil) {
+
+    private lateinit var itemClickListener: OnItemClickListener
+
+    class AddressViewHolder(
+        val context: Context,
+        val binding: ItemAddressBinding,
+        val listener: OnItemClickListener,
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: AddressViewModel.Address) = binding.apply {
+            roadTxt.text = "${item.landNumber} ${item.roadName}"
+            if (item.buildingName != null) {
+                buildingNameTxt.text = item.buildingName
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddressViewHolder =
+        AddressViewHolder(
+            parent.context,
+            ItemAddressBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ),
+            itemClickListener
+        )
+
+    override fun onBindViewHolder(holder: AddressViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    interface OnItemClickListener {
+        fun click(item: AddressViewModel.Address)
+    }
+
+    fun setItemOnClickListener(onItemClickListener: OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<AddressViewModel.Address>() {
+            override fun areItemsTheSame(
+                oldItem: AddressViewModel.Address,
+                newItem: AddressViewModel.Address
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: AddressViewModel.Address,
+                newItem: AddressViewModel.Address
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
