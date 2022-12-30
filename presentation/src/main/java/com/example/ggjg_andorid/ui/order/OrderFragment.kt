ce56a475cef52314ecbfdd137920fbc979ba566f
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-30 20:52:13 +0900

    주문 리스트에서 클릭 이벤트에 따라 페이지 이동

---
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/order/OrderFragment.kt
@@ -0,0 +1,84 @@
+package com.example.ggjg_andorid.ui.order
+
+import android.content.Context
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.domain.entity.order.MyOrderListEntity
+import com.example.ggjg_andorid.R
+import com.example.ggjg_andorid.adapter.OrderAdapter
+import com.example.ggjg_andorid.databinding.FragmentOrderBinding
+import com.example.ggjg_andorid.ui.base.BaseFragment
+import com.example.ggjg_andorid.utils.repeatOnStart
+import com.example.ggjg_andorid.viewmodel.MainViewModel
+import com.example.ggjg_andorid.viewmodel.OrderViewModel
+
+class OrderFragment : BaseFragment<FragmentOrderBinding>(R.layout.fragment_order) {
+    private val orderViewModel by activityViewModels<OrderViewModel>()
+    private val mainViewModel by activityViewModels<MainViewModel>()
+    private lateinit var orderAdapter: OrderAdapter
+
+    override fun onDetach() {
+        mainViewModel.hiddenNav(false)
+        super.onDetach()
+    }
+
+    override fun onAttach(context: Context) {
+        mainViewModel.hiddenNav(true)
+        super.onAttach(context)
+    }
+
+    override fun createView() {
+        binding.order = this
+        initView()
+        repeatOnStart {
+            orderViewModel.eventFlow.collect { event -> handleEvent(event) }
+        }
+    }
+
+    private fun handleEvent(event: OrderViewModel.Event) = when (event) {
+        is OrderViewModel.Event.OrderList -> {
+            orderAdapter.submitList(event.data)
+        }
+    }
+
+    private fun initView() = binding.apply {
+        orderViewModel.myOrderList()
+        orderAdapter = OrderAdapter().apply {
+            setItemOnClickListener(object : OrderAdapter.OnItemClickListener {
+                override fun detail(item: MyOrderListEntity) {
+                    requireActivity().findNavController(R.id.mainContainer)
+                        .navigate(R.id.action_orderFragment_to_detailOrderFragment)
+                }
+
+                override fun cancel(item: MyOrderListEntity) {
+                    requireActivity().findNavController(R.id.mainContainer)
+                        .navigate(R.id.action_orderFragment_to_orderCancelFragment)
+                }
+
+                override fun recall(item: MyOrderListEntity) {
+                    requireActivity().findNavController(R.id.mainContainer)
+                        .navigate(R.id.action_orderFragment_to_orderRecallFragment)
+                }
+
+                override fun review(item: MyOrderListEntity) {
+                    requireActivity().findNavController(R.id.mainContainer)
+                        .navigate(R.id.action_orderFragment_to_orderReviewFragment)
+                }
+            })
+        }
+        orderList.apply {
+            adapter = orderAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.backBtn -> {
+                requireActivity().findNavController(R.id.mainContainer).popBackStack()
+            }
+        }
+    }
+}
