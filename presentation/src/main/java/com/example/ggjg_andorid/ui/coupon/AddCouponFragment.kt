429aece5786b68925b4eceb839b9176e9d8a6d43
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-31 16:09:39 +0900

    쿠폰 추가 기능 구현

--- presentation/src/main/java/com/example/ggjg_andorid/ui/coupon/AddCouponFragment.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/coupon/AddCouponFragment.kt
@@ -2,17 +2,22 @@ package com.example.ggjg_andorid.ui.coupon
 
 import android.view.View
 import android.widget.ScrollView
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ggjg_andorid.R
 import com.example.ggjg_andorid.adapter.AddCouponAdapter
 import com.example.ggjg_andorid.databinding.FragmentCouponAddBinding
 import com.example.ggjg_andorid.ui.base.BaseFragment
+import com.example.ggjg_andorid.utils.keyboardHide
+import com.example.ggjg_andorid.utils.repeatOnStart
 import com.example.ggjg_andorid.utils.setVisible
 import com.example.ggjg_andorid.viewmodel.CouponViewModel
 
 class AddCouponFragment : BaseFragment<FragmentCouponAddBinding>(R.layout.fragment_coupon_add) {
     private lateinit var addCouponAdapter: AddCouponAdapter
+    private val couponViewModel by activityViewModels<CouponViewModel>()
 
     override fun onDetach() {
         CouponViewModel.apply {
@@ -26,6 +31,26 @@ class AddCouponFragment : BaseFragment<FragmentCouponAddBinding>(R.layout.fragme
         binding.addCoupon = this
         CouponViewModel.couponList = listOf("")
         initView()
+        repeatOnStart {
+            couponViewModel.addEventFlow.collect { event -> handleEvent(event) }
+        }
+    }
+
+    private fun handleEvent(event: CouponViewModel.AddEvent) = when (event) {
+        is CouponViewModel.AddEvent.NoneCoupon -> {
+            requireActivity().findNavController(R.id.mainContainer).popBackStack()
+        }
+        is CouponViewModel.AddEvent.WrongCoupon -> {
+            keyboardHide(requireActivity(), listOf())
+            Toast.makeText(context, "${event.errorCnt}개의 쿠폰 번호가 유효하지 않습니다.", Toast.LENGTH_SHORT)
+                .show()
+            requireActivity().findNavController(R.id.mainContainer).popBackStack()
+        }
+        is CouponViewModel.AddEvent.SuccessCoupon -> {
+            keyboardHide(requireActivity(), listOf())
+            Toast.makeText(context, "쿠폰 입력에 성공했습니다.", Toast.LENGTH_SHORT).show()
+            requireActivity().findNavController(R.id.mainContainer).popBackStack()
+        }
     }
 
     private fun initView() = binding.apply {
@@ -63,6 +88,9 @@ class AddCouponFragment : BaseFragment<FragmentCouponAddBinding>(R.layout.fragme
                 if (CouponViewModel.couponList.size == 3) {
                     binding.addCouponBtn.setVisible(false)
                 }
+            }
+            R.id.enrollBtn -> {
+                couponViewModel.enrollCoupon()
             }
         }
     }
--- presentation/src/main/java/com/example/ggjg_andorid/viewmodel/CouponViewModel.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/viewmodel/CouponViewModel.kt
@@ -1,14 +1,53 @@
 package com.example.ggjg_andorid.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.usecase.coupon.EnrollCouponUseCase
+import com.example.ggjg_andorid.utils.MutableEventFlow
+import com.example.ggjg_andorid.utils.asEventFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CouponViewModel @Inject constructor(
+    private val enrollCouponUseCase: EnrollCouponUseCase,
 ) : ViewModel() {
     companion object {
         var couponList = listOf<String>()
         var currentPosition = 0
+    }
+
+    private val _addEventFlow = MutableEventFlow<AddEvent>()
+    val addEventFlow = _addEventFlow.asEventFlow()
+
+    fun enrollCoupon() = viewModelScope.launch {
+        var errorCnt = 0
+        if (couponList.size == 1 && couponList.contains("")) {
+            event(AddEvent.NoneCoupon)
+        } else {
+            couponList.forEach {
+                kotlin.runCatching {
+                    enrollCouponUseCase.execute(it)
+                }.onFailure {
+                    errorCnt++
+                }
+            }
+            if (errorCnt != 0) {
+                event(AddEvent.WrongCoupon(errorCnt))
+            } else {
+                event(AddEvent.SuccessCoupon)
+            }
+        }
+    }
+
+    private fun event(event: AddEvent) = viewModelScope.launch {
+        _addEventFlow.emit(event)
+    }
+
+    sealed class AddEvent {
+        data class WrongCoupon(val errorCnt: Int) : AddEvent()
+        object SuccessCoupon : AddEvent()
+        object NoneCoupon : AddEvent()
     }
 }
