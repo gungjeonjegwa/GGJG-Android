6a4454de9bfb460d32c013189e94a0da6efea317
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-31 20:45:49 +0900

    검색 중일 때 바텀 네비게이션 숨기기

--- presentation/src/main/java/com/example/ggjg_andorid/ui/search/SearchingFragment.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/search/SearchingFragment.kt
@@ -1,11 +1,14 @@
 package com.example.ggjg_andorid.ui.search
 
+import androidx.fragment.app.activityViewModels
 import com.example.ggjg_andorid.R
 import com.example.ggjg_andorid.databinding.FragmentSearchingBinding
 import com.example.ggjg_andorid.ui.base.BaseFragment
+import com.example.ggjg_andorid.viewmodel.MainViewModel
 
 class SearchingFragment : BaseFragment<FragmentSearchingBinding>(R.layout.fragment_searching) {
+    private val mainViewModel by activityViewModels<MainViewModel>()
     override fun createView() {
-
+        mainViewModel.hiddenNav(true)
     }
 }
