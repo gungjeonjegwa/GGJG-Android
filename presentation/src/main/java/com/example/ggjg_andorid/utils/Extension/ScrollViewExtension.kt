6ecc3dec28ba35a5add39a5e7fb3f771ac0afde6
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-31 12:40:10 +0900

    스크롤 뷰 커스텀 스크롤 작성

---
+++ presentation/src/main/java/com/example/ggjg_andorid/utils/Extension/ScrollViewExtension.kt
@@ -0,0 +1,19 @@
+package com.example.ggjg_andorid.utils.Extension
+
+import android.animation.ObjectAnimator
+import android.view.View
+import androidx.core.widget.NestedScrollView
+import kotlin.math.abs
+
+fun NestedScrollView.customTopScroll(clickView: View) {
+    if (this.getChildAt(0).height <= this.height) return
+    val ratio = abs(0 - this.scrollY) / (this.getChildAt(0).height - this.height).toFloat()
+    val scrollAnimation = ObjectAnimator.ofInt(this, "scrollY", 0).apply {
+        duration = (1000L * ratio).toLong()
+    }
+    clickView.setOnTouchListener { _, _ ->
+        scrollAnimation.pause()
+        false
+    }
+    scrollAnimation.start()
+}
