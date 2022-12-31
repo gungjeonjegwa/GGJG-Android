3299cd95d4cf02435bb53ccfe84bf0b5f41fd30e
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-31 13:08:56 +0900

    주문 취소 라디오 버튼 클릭 이벤트 기능 구현

--- presentation/src/main/java/com/example/ggjg_andorid/ui/order/OrderCancelFragment.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/ui/order/OrderCancelFragment.kt
@@ -5,17 +5,31 @@ import androidx.navigation.findNavController
 import com.example.ggjg_andorid.R
 import com.example.ggjg_andorid.databinding.FragmentOrderCancelBinding
 import com.example.ggjg_andorid.ui.base.BaseFragment
+import com.example.ggjg_andorid.utils.changeActivatedWithEnabled
 
 class OrderCancelFragment :
     BaseFragment<FragmentOrderCancelBinding>(R.layout.fragment_order_cancel) {
     override fun createView() {
         binding.orderCancel = this
+        initView()
+    }
+
+    private fun initView() = binding.apply {
+
     }
 
     fun onClick(view: View) {
-        when (view.id) {
-            R.id.backBtn -> {
-                requireActivity().findNavController(R.id.mainContainer).popBackStack()
+        binding.apply {
+            when (view) {
+                backBtn -> {
+                    requireActivity().findNavController(R.id.mainContainer).popBackStack()
+                }
+                justBtn, couponBtn, optionBtn, delayBtn, addressBtn, etcBtn -> {
+                    listOf(justBtn, couponBtn, optionBtn, delayBtn, addressBtn, etcBtn).forEach {
+                        it.isSelected = it == view
+                    }
+                    cancelBtn.changeActivatedWithEnabled(true)
+                }
             }
         }
     }
