fa9f702e390f149f78f9eda999f3da1169c8e4eb
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-31 10:07:52 +0900

    최근 배송지 기능 구현

--- presentation/src/main/java/com/example/ggjg_andorid/viewmodel/AddressViewModel.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/viewmodel/AddressViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.data.utils.removeDot
 import com.example.domain.model.AddressModel
+import com.example.domain.usecase.auth.RecentAddressUseCase
 import com.example.ggjg_andorid.utils.MutableEventFlow
 import com.example.ggjg_andorid.utils.asEventFlow
 import com.google.gson.JsonArray
@@ -17,16 +18,17 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AddressViewModel @Inject constructor(
-
+    private val recentAddressUseCase: RecentAddressUseCase,
 ) : ViewModel() {
     private val _searchEventFlow = MutableEventFlow<SearchEvent>()
     val searchEventFlow = _searchEventFlow.asEventFlow()
+    private val _changeEventFlow = MutableEventFlow<ChangeEvent>()
+    val changeEventFlow = _changeEventFlow.asEventFlow()
 
     companion object {
         var currentAddress: AddressModel? = null
         var isClickSearch = false
         var isPayment = true
-        var buildingList = listOf<String>()
     }
 
     fun search(query: String) = viewModelScope.launch {
@@ -69,8 +71,24 @@ class AddressViewModel @Inject constructor(
         })
     }
 
+    fun recentSearch() = viewModelScope.launch {
+        kotlin.runCatching {
+            recentAddressUseCase.execute()
+        }.onSuccess {
+            if (it.isNotEmpty()) {
+                event(ChangeEvent.RecentSearch((it)))
+            } else {
+                event(ChangeEvent.NoSearch)
+            }
+        }
+    }
+
     private fun event(event: SearchEvent) = viewModelScope.launch {
         _searchEventFlow.emit(event)
+    }
+
+    private fun event(event: ChangeEvent) = viewModelScope.launch {
+        _changeEventFlow.emit(event)
     }
 
     sealed class SearchEvent {
@@ -78,6 +96,7 @@ class AddressViewModel @Inject constructor(
     }
 
     sealed class ChangeEvent {
-
+        data class RecentSearch(val data: List<AddressModel>) : ChangeEvent()
+        object NoSearch : ChangeEvent()
     }
 }
