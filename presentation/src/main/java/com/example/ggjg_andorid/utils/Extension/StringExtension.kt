2d6be5c87a925be9cf8e7d2ae67f093c11b045c9
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-01-01 12:48:52 +0900

    날짜와 요일을 반환하는 Extension 작성

--- presentation/src/main/java/com/example/ggjg_andorid/utils/Extension/StringExtension.kt
+++ presentation/src/main/java/com/example/ggjg_andorid/utils/Extension/StringExtension.kt
@@ -2,8 +2,11 @@ package com.example.ggjg_andorid.utils
 
 import android.util.Patterns
 import java.io.File
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.*
 import java.util.regex.Pattern
 
 fun String.isPhoneNumber(): Boolean =
@@ -29,6 +32,12 @@ fun String.toDeliveryType(): String = when (this) {
 fun String.toDate(): String {
     val date = LocalDate.parse(this.slice(0..9), DateTimeFormatter.ISO_DATE)
     return date.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일"))
+}
+
+fun String.toOrderDate(): String {
+    val date = LocalDate.parse(this.slice(0..9), DateTimeFormatter.ISO_DATE)
+    val dayOfWeek = date.dayOfWeek
+    return "${date.format(DateTimeFormatter.ofPattern("MM월 dd일"))} (${dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN)})"
 }
 
 fun String.getDirectoryPaths(): List<String> {
