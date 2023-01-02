1ecefd1a3c50871ab461aaa18a2670dc4215dadb
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-01-02 21:46:40 +0900

    최근 검색어 중복 검색시 교체

--- data/src/main/java/com/example/data/local/dao/RecentSearchDao.kt
+++ data/src/main/java/com/example/data/local/dao/RecentSearchDao.kt
@@ -2,12 +2,13 @@ package com.example.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.example.data.local.entity.RecentSearchEntity
 
 @Dao
 interface RecentSearchDao {
-    @Insert
+    @Insert(onConflict = REPLACE)
     suspend fun searchBread(recentSearchEntity: RecentSearchEntity)
 
     @Query("DELETE FROM recent_search WHERE search = :search")
