17dbda6de7d09d0cd7461e551eecd46c1989e710
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-03-02 12:09:20 +0900

    토큰 저장 기본 값 추가

--- domain/src/main/java/com/example/domain/usecase/auth/SaveTokenUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/auth/SaveTokenUseCase.kt
@@ -6,7 +6,7 @@ import javax.inject.Inject
 class SaveTokenUseCase @Inject constructor(
     private val authRepository: AuthRepository,
 ) {
-    suspend operator fun invoke(access: String?, refresh: String?, expiredAt: String?) =
+    suspend operator fun invoke(access: String? = null, refresh: String? = null, expiredAt: String? = null) =
         kotlin.runCatching {
             authRepository.saveToken(access, refresh, expiredAt)
         }
