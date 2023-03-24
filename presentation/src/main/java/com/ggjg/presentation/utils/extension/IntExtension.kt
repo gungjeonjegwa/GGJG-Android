101e41606bf086a56da8f6142dd5e61fecb6bd5f
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-03-24 14:37:03 +0900

    ind to dp 함수 추가

--- presentation/src/main/java/com/ggjg/presentation/utils/extension/IntExtension.kt
+++ presentation/src/main/java/com/ggjg/presentation/utils/extension/IntExtension.kt
@@ -1,4 +1,8 @@
 package com.ggjg.presentation.utils.extension
 
+import android.content.res.Resources
+
 fun Int.toTotalMoney(extraMoney: Int?, count: Int): Int =
     (this + (extraMoney ?: 0)) * count
+
+fun Int.toDp(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
